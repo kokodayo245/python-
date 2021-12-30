88c46807e272ce93d2382b59d1a0ae7eff50d34b
kokodayo245 <41479064+kokodayo245@users.noreply.github.com>
2021-12-30 18:14:24 +0800

    Add files via upload

--- study/Part8 Class/car.py
+++ study/Part8 Class/car.py
@@ -23,7 +23,6 @@ class Car(object):
         self.odometer_reading += miles
 
 
-# 继承 父类Car
 class ElectricCar(Car):
     def __init__(self, make, model, year):
         # 初始化父类的属性，再初始化电动汽车特有的属性
@@ -32,15 +31,3 @@ class ElectricCar(Car):
 
     def describe_battery(self):
         print("This car has a " + str(self.battery_size) + "-kwh battery.")
-
-
-my_tesla = ElectricCar('tesla', 'model s', 2016)
-print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
-
-my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
-my_new_car.odometer_reading = 22
-my_new_car.update_odometer(25)
-my_new_car.update_odometer(20)
-my_new_car.read_odometer()
